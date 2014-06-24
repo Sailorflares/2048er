4f0227d430aa53669b0ad54e8edd7b084c75a3e5
Sailorflares <liz.wolensky@gmail.com>
2014-06-23 22:20:19 -0400

    Board can be initialized to all 0's and randomly placed 2's. Values on the board can be shifted to left.

--- 2048.js
+++ 2048.js
@@ -1,3 +1,87 @@
-var additionFunction = function( first, second ){
-	return first + second;
+var printBoard = function(array) {
+  var stringyString = "";
+  for (i=0; i<4; i++){
+    for (j=0; j<4; j++){
+      stringyString = stringyString + " " + array[i][j];
+    }
+    stringyString = stringyString + "\n";
+  }
+  console.log(stringyString);
 }
+
+var intializeBoard = function() {
+  var playerBoard = [ [ 0,0,0,0 ],[ 0,0,0,0 ],[ 0,0,0,0 ],[ 0,0,0,0 ] ];
+  return playerBoard;
+}
+
+var setRandomSpotToTwo = function(array) {
+  var i = Math.round(Math.random()*3);
+  var j = Math.round(Math.random()*3);
+  array[i][j] = 2;
+}
+
+var shiftValuesLeftByOneSpace = function(array,i,j) {
+  for (j; j<3; j++){
+    array[i][j] = array[i][j+1];
+  }
+  array[i][3] = 0;
+  return array;
+}
+
+var shiftValuesLeftByGivenNumber = function(array,i,j,count){
+  for (l=0; l<count; l++){
+    array = shiftValuesLeftByOneSpace(array,i,j);
+  }
+  return array;
+}
+
+var moveNonZeroValuesToLeft = function (array){
+  for (i=0; i<array.length; i++){
+    for (j=0; j<array.length; j++){
+      if (array[i][j]==0){
+        var count=1;
+        for (k=j+1; k<array.length; k++){
+          if (array[i][k]==0){
+            count++;          
+          }
+          else{
+            k=array.length;
+          } 
+        }
+        array = shiftValuesLeftByGivenNumber(array,i,j,count);
+      }  
+    }
+  } 
+  return array;
+}
+
+
+//var funBoard = intializeBoard();
+//printBoard(funBoard);
+
+//var isNonZeroValuesColumn = function(array[i][j]){
+  //This will return true or false, depending on whether there are non-zero values in below column spots, might take an array length and an i value
+//}
+
+/*
+var upKeyFunction = function(array) {
+  for (j=0; j<4; j++){
+    for (i=0; i<4; i++){
+      while (isNonZeroValuesColumn()==true){
+        if (array[i][j]==0){
+          for (k=i; k<4; k++){
+            array[i][j] = array[i+1][j];
+            array[i+1][j] = array[i+2][j];
+            array[i+2][j] = array[i+3][j];
+            array[i+3][j] = 0;
+          
+          for (k=i;k<array.length-1;k++){
+            
+          }
+        }  
+      }
+    }
+  }  
+}
+*/
+
--- tests.js
+++ tests.js
@@ -1,4 +1,85 @@
-test( "addition of two numbers", function() {
-  equal( additionFunction(2,2), 4, "2 plus 2 should equal 4" );
-  equal( additionFunction(8,8), 16, "8 plus 8 should equal 16" );
+test( "two numbers placed randomly", function() {
+	sinon.stub( Math, 'random' ).returns(0);
+	var array = [ [],[],[],[] ];
+	array[0][0] = "Blank";
+	
+	setRandomSpotToTwo(array);
+
+	equal( array[0][0], 2, "Value at array[0][0] should be 2" );
+});
+
+test( "board filled with zeros", function() {
+	var array = intializeBoard();
+
+	for (i=0; i<array.length; i++) {
+		for (j=0; j<array[i].length; j++) {
+			equal( array[i][j], 0, "Value at board spot (" + i + "," + j + ") should equal 0" );
+		}
+	}
+});
+
+test( "shift values left by one space, starting at index 0 on the first row", function() {
+	var array1 = [0,2,0,2];
+	var array2 = [0,0,2,2];
+	var array3 = [2,0,2,0];
+	var array4 = [2,0,0,0];
+	var array = [array1,array2,array3,array4];
+
+	var result = shiftValuesLeftByOneSpace(array, 0, 0);
+	
+	deepEqual ( result, [[2,0,2,0],[0,0,2,2],[2,0,2,0],[2,0,0,0]], "See test" );
+});
+
+test( "shift values left by one space, starting at index 1 on the third row", function() {
+	var array1 = [0,2,0,2];
+	var array2 = [0,0,2,2];
+	var array3 = [2,0,2,0];
+	var array4 = [2,0,0,0];
+	var array = [array1,array2,array3,array4];
+
+	var result = shiftValuesLeftByOneSpace(array, 2, 1);
+	
+	deepEqual ( result, [[0,2,0,2],[0,0,2,2],[2,2,0,0],[2,0,0,0]], "See test" );
+});
+
+test( "move all nonzero values to the left", function() {
+	var array1 = [0,2,0,2];
+	var array2 = [0,0,2,2];
+	var array3 = [2,0,2,0];
+	var array4 = [2,0,0,0];
+	var array = [array1,array2,array3,array4];
+
+	var result = moveNonZeroValuesToLeft(array);
+	
+	deepEqual ( result, [[2,2,0,0],[2,2,0,0],[2,2,0,0],[2,0,0,0]], "See test" );
+});
+
+test( "shift values left 2 times, starting at first spot", function() {
+	var array1 = [0,0,2,2];
+	var array2 = [2,0,2,0];
+	var array = [array1, array2];
+
+	var result = shiftValuesLeftByGivenNumber(array, 0, 0, 2);
+	
+	deepEqual ( result, [[2,2,0,0],[2,0,2,0]], "See test" );
+});
+
+test( "shift values left 1 time, starting at second spot", function() {
+	var array1 = [0,0,2,2];
+	var array2 = [2,0,2,0];
+	var array = [array1, array2];
+
+	var result = shiftValuesLeftByGivenNumber(array, 0, 1, 1);
+	
+	deepEqual ( result, [[0,2,2,0],[2,0,2,0]], "See test" );
+});
+
+test( "shift values left 1 time, starting at second spot in second array", function() {
+	var array1 = [0,0,2,2];
+	var array2 = [2,0,2,0];
+	var array = [array1, array2];
+
+	var result = shiftValuesLeftByGivenNumber(array, 1, 1, 1);
+	
+	deepEqual ( result, [[0,0,2,2,],[2,2,0,0]], "See test" );
 });
